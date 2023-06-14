2f7710f69c7c17c50a225138a8f52636a1454cdf
Armen Hovhannisyan <a.hov@mail.ru>
2023-06-14 21:14:41 +0400

    My first git commit

---
+++ Leson1/src/Test1.java
@@ -0,0 +1,41 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone,");
+        System.out.println("We start learn lesson 1");
+        byte x = 7;
+        System.out.println(x);
+
+        short y = -4590;
+        System.out.println(y);
+
+        int z = 568;
+        System.out.println(z);
+
+        long e = 45806984989l;
+        System.out.println(e);
+// kamayakan amboxj tiv int e
+        float a = 455449897.95F;
+        System.out.println(a);
+        double b = 45.95;
+        System.out.println(b);
+// kamayakan racional tiv doubl e
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        char t = '@';
+        char tt = '\u003F';
+        System.out.println(t);
+        System.out.println(tt);
+
+        String firstName = "Armen";
+        String lastName = "Hovhannisyan";
+        System.out.println(firstName + " " + lastName);
+
+        int kk =95;
+        System.out.println("k ="+" " +kk);
+
+    }
+}
